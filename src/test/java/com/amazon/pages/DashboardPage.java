3e8f2d6e129256e18f23c6ce806ecb7a41058826
Miguel Angel Peceros Vargas <mapevar2489@gmail.com>
2020-11-02 12:21:06 -0500

    feature/exerciseCompleted
    exercise was completed

--- src/test/java/com/amazon/pages/DashboardPage.java
+++ src/test/java/com/amazon/pages/DashboardPage.java
@@ -3,6 +3,10 @@ package com.amazon.pages;
 import com.framework.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DashboardPage extends BasePage {
 
@@ -12,6 +16,10 @@ public class DashboardPage extends BasePage {
 
     By searchTexControl = By.cssSelector("#twotabsearchtextbox");
     By searchTexButton = By.cssSelector("#nav-search-submit-text");
+    By searchResultItems = By.cssSelector(".s-result-item");
+    By bestSellerItems = By.xpath("//span[contains(@id, 'best-seller-label')]");
+    String xPathItemsFilteredByBadgeName = "//div[contains(@class, 's-result-item')]//span[contains(., 'badgeName') " +
+            "and contains(@class, 'a-badge-text')]/ancestor::div[contains(@class, 's-result-item')]";
 
     public void openDashboard(){
         loadPage("https://www.amazon.com/");
@@ -21,7 +29,29 @@ public class DashboardPage extends BasePage {
         sendValues(searchTexControl,text);
     }
 
-    public void searchProducts(){
+    public void searchItems(){
         clickOnElement(searchTexButton);
+    }
+
+    public int getListOfItems(){
+        return getListOfElementsBy(searchResultItems).size();
+    }
+
+    public int getListOfProductsWithBadge(String badge){
+        String locator = xPathItemsFilteredByBadgeName.replace("badgeName", badge);
+        return getListOfElementsBy(By.xpath(locator)).size();
+    }
+
+    public void listFilteredItems(String badge, String item) {
+        String locator = xPathItemsFilteredByBadgeName.replace("badgeName", badge);
+        List<WebElement> webElementList = getListOfElementsBy(By.xpath(locator));
+        AtomicInteger i = new AtomicInteger(1);
+        if (webElementList.size() > 0)
+            webElementList.forEach((final WebElement e) -> {
+                takeWebElementScreenShot(e, item+"_"+badge.concat(String.valueOf(i.get())));
+                i.getAndIncrement();
+            });
+        else
+            log.error("Items not Found for "+item+ " with badge "+badge+" in the first page");
     }
 }
